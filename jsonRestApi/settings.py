2b60c72412a409efd363c4155ae1bbf42853501c
KwakyCodes <emmanuelkwakyenyantakyi@gmail.com>
2022-06-15 00:14:59 +0000

    .

--- jsonRestApi/settings.py
+++ jsonRestApi/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.contrib.gis.db.backends.mysql',
         'NAME': os.environ.get('DB_NAME'),
         'HOST': os.environ.get('DB_HOST'),
-        'PORT': '5432',
+        'PORT': '3306',
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
     }
